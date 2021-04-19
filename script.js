88fcc005e8c3471023b84fdbea815df6b7a04d59
taejin <taejin5314@gmail.com>
2021-04-19 17:57:45 -0400

    making a file choose button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.getElementById('container');
 const canvas = document.getElementById('canvas1');
+const file = document.getElementById('fileupload')
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
