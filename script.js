2beaac5a7cfbe8ea3bbda40e277f029437700e11
taejin <taejin5314@gmail.com>
2021-04-19 00:38:24 -0400

    add a song and play the song when button clicked

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const button1 = document.getElementById('button1');
+let audio1 = new Audio();
+audio1.src = 'stone.mp3';
 
 button1.addEventListener('click', function () {
-  console.log('click')
+  audio1.play();
 })
