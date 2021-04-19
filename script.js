8e1d65db56937acf1aa3dd61c3f48fb5fd4d6766
taejin <taejin5314@gmail.com>
2021-04-19 18:05:57 -0400

    file choose -> play the music selected

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ container.addEventListener('click', function () {
   const audio1 = document.getElementById('audio1');
   audio1.src = 'stone.mp3';
   const audioCtx = new AudioContext();
-  audio1.play();
+  // audio1.play();
   audioSource = audioCtx.createMediaElementSource(audio1);
   analyser = audioCtx.createAnalyser();
   audioSource.connect(analyser);
@@ -29,13 +29,46 @@ container.addEventListener('click', function () {
     x = 0;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     analyser.getByteFrequencyData(dataArray);
-    for (let i = 0; i < bufferLength; i++) {
-      barHeight = dataArray[i];
-      ctx.fillStyle = 'white';
-      ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-      x += barWidth;
-    }
+    drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray);
     requestAnimationFrame(animate);
   }
   animate();
 });
+
+file.addEventListener('change', function () {
+  const files = this.files;
+  const audio1 = document.getElementById('audio1');
+  audio1.src = URL.createObjectURL(files[0]);
+  audio1.load();
+  audio1.play();
+
+  audioSource = audioCtx.createMediaElementSource(audio1);
+  analyser = audioCtx.createAnalyser();
+  audioSource.connect(analyser);
+  analyser.connect(audioCtx.destination);
+  analyser.fftSize = 64;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  const barWidth = canvas.width / bufferLength;
+  let barHeight;
+  let x;
+
+  function animate() {
+    x = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    analyser.getByteFrequencyData(dataArray);
+    drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray);
+    requestAnimationFrame(animate);
+  }
+  animate();
+})
+
+function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray) {
+  for (let i = 0; i < bufferLength; i++) {
+    barHeight = dataArray[i] * 2;
+    ctx.fillStyle = 'white';
+    ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+    x += barWidth;
+  }
+}
